25e8d3ee6fdf178e47adb113dde40fa5f304cbb4
Raissa Vicente <raissa.carvalho@zup.com.br>
2023-07-14 11:16:28 -0300

    Atualização

--- src/main/kotlin/zoologico/Main.kt
+++ src/main/kotlin/zoologico/Main.kt
@@ -1,2 +1,24 @@
 package zoologico
 
+fun main(){
+    var entrada = false
+    while (!entrada){
+        println("Menu")
+        println("1. Cadastrar | 2. Remover | 3. Listar | 4. Interagir | 5. Sair")
+        println("Digite a opção desejada:")
+        val opcao = readlnOrNull()?.toIntOrNull() ?: 0
+
+        when (opcao) {
+            1 -> println()
+            2 -> println()
+            3 -> println()
+            4 -> println()
+            5 -> {
+                entrada = true
+                println("Saindo do programa...")
+            }
+            else -> println("Opção inválida. Tente novamente.")
+        }
+        println()
+    }
+}
